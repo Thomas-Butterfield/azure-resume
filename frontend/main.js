b1b1dc15e64f2cd4232de41cb624c7b7b616366c
Thomas Butterfield <107024907+Thomas-Butterfield@users.noreply.github.com>
2022-11-26 17:04:03 -0700

    main js

--- frontend/main.js
+++ frontend/main.js
@@ -5,15 +5,15 @@ window.addEventListener('DOMContentLoaded', (event) =>{
 const functionApiUrl = 'https://func-butterresume.azurewebsites.net/api/Counter?code=SEuykv0ePEJ1rLNymzyiKTH60rV0dUM4beKwpdvgFiJFAzFui7H32Q==';
 
 const getVisitCount = () => {
-    let count = 30;
+    let visitorCount = 30;
     fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response =>{
         console.log("Website called function API.");
-        count =  response.count;
+        count = response.VisitorCount;
         document.getElementById("counter").innerText = count;
     }).catch(function(error){
         console.log(error);
     });
-    return count;
+    return visitorCount;
 }
